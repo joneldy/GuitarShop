b029d1efa8e19521d889c91d7c5cd0e54665b5e1
jondy14 <jonel.cabotaje@decenternet.net>
2020-06-20 18:40:26 +0800

    Adding filter guitars

--- server/server.js
+++ server/server.js
@@ -37,6 +37,44 @@ const { admin } = require('./middleware/admin');
   Products
 */
 
+app.post('/api/product/shop', (req, res) => {
+  let order = req.body.order ? req.body.order : 'desc';
+  let sortBy = req.body.sortBy ? req.body.sortBy : '_id';
+  let limit = req.body.limit ? parseInt(req.body.limit) : 100;
+  let skip = parseInt(req.body.skip);
+  let findArgs = {};
+
+  for (let key in req.body.filters) {
+    if (req.body.filters[key].length > 0) {
+      if (key === 'price') {
+        findArgs[key] = {
+          $gte: req.body.filters[key][0],
+          $lte: req.body.filters[key][1],
+        };
+      } else {
+        findArgs[key] = req.body.filters[key];
+      }
+    }
+  }
+
+  console.log('findArgs', findArgs);
+  findArgs['publish'] = true;
+
+  Product.find(findArgs)
+    .populate('brand')
+    .populate('wood')
+    .sort([[sortBy, order]])
+    .skip(skip)
+    .limit(limit)
+    .exec((err, articles) => {
+      if (err) return res.status(400).send(err);
+      res.status(200).json({
+        size: articles.length,
+        articles,
+      });
+    });
+});
+
 // BY ARRIVAL
 // /articles?sortBy=createdAt&order=desc&limit=4
 
@@ -184,7 +222,7 @@ app.post('/api/users/login', (req, res) => {
   });
 });
 
-app.get('/api/user/logout', auth, (req, res) => {
+app.get('/api/users/logout', auth, (req, res) => {
   User.findOneAndUpdate({ _id: req.user._id }, { token: '' }, (err, doc) => {
     if (err) return res.json({ success: false, err });
     return res.status(200).send({
